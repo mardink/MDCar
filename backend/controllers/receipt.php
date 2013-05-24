06c736151b0ca7db8ea692dee9e5c6f386824815
Mardink Webdesign <support@mardinkwebdesign.com>
2013-05-24 22:20:45 +0200

    Connection between cars and receipts working for fuel
    Doesn't show one of the cars anymore. Why? Try creating an extra car

--- backend/controllers/receipt.php
+++ backend/controllers/receipt.php
@@ -10,10 +10,28 @@ defined('_JEXEC') or die();
 
 class MdcarControllerReceipt extends FOFController
 {
-    public function onBeforeSave() {
-        $fuel = 'Lullo';
-        $this->item->fuel = $fuel;
-        return $this->item->fuel;
+    public function onAfterSave() {
+        $model = $this->getThisModel();
+        $model->setIDsFromRequest();
+        $id = $model->getId();
+        $mdcar_car_id = $model->mdcar_car_id;
+
+        // Get fuel type form car database
+        $md_fuel = new MdcarModelReceipts;
+        $fuel = $md_fuel->Fuel($mdcar_car_id);
+
+        //get variables and store
+        $item = $model->getItem();
+        $key = $item->getKeyName();
+
+        if($item->$key == $id)
+        {
+            $item->id = $id;
+            $item->fuel = $fuel;
+                    }
+        $status = $model->save($item);
+
+        return true;
     }
 
 }
--- backend/models/receipts.php
+++ backend/models/receipts.php
@@ -8,7 +8,25 @@
 // Protect from unauthorized access
 defined('_JEXEC') or die();
 
-class MdcarModelReceipt extends FOFModel
+class MdcarModelReceipts extends FOFModel
 {
+public function Fuel($mdcar_car_id){
+// setup a database connection
+    $id = $mdcar_car_id;
+    $db = $this->getDbo();
 
+    $query = $db->getQuery(true)
+        ->select('fuel')
+        ->from($db->quoteName('#__mdcar_cars'))
+        ->where($db->qn('mdcar_car_id').' = '.$db->q($id));
+    // Reset the query using our newly populated query object.
+    $db->setQuery($query);
+
+// Load the results as a list of stdClass objects.
+    $result = $db->loadObject();
+    //$Fuel_car = 'dieseltje';
+    $Fuel_car = $result->fuel;
+
+    return $Fuel_car;
+}
 }
